b30b2dfbd7a35891115926c71007773f29be9fcd
elkira01 <elkira01@gmail.com>
2025-05-08 11:18:37 +0100

    refactor

--- src/api/axios/index.ts
+++ src/api/axios/index.ts
@@ -6,8 +6,9 @@ const AxiosInstance = axios.create({
   baseURL: "http://localhost:3333/api",
 });
 
+const token = localStorage.getItem("app-access-token") ?? "";
 AxiosInstance.defaults.headers.common["Authorization"] =
-  `Bearer ${JSON.parse(localStorage.getItem("app-access-token") ?? "")}`;
+  `Bearer ${JSON.parse(JSON.stringify(token))}`;
 
 // export const callAction = (endPoint: string, method: axios.Method)
 
